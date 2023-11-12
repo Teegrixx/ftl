ac9aa679874cdd5117a9fe5c7e6031ca34fe3f67
teegrixx <89885361+Teegrixx@users.noreply.github.com>
2023-11-12 16:38:16 +0300

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -17,12 +17,12 @@ async def maintainers(b,m):
     except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="I am Made By [Rk_botz](https://t.me/Rk_botz)",
+                    text="I am Made By [GRIXXTECH](https://t.me/+jdSCugazgfxmYjNk",
                     
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("Owner💻", url=f"https://t.me/Rk_botowner")
+                                InlineKeyboardButton("Owner💻", url=f"https://t.me/teegrixx")
                             ]
                         ]
                     ),
@@ -42,7 +42,7 @@ async def follow_user(b,m):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("Subscribe ❤️", url=f"https://t.me/Rk_update")
+                                InlineKeyboardButton("Subscribe ❤️", url=f"https://t.me/+jdSCugazgfxmYjNk")
                             ]
                         ]
                     ),
